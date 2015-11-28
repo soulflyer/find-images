2c30a58446023b98c1282848c4647ef17d28c712
iain <iain@soulflyer.co.uk>
2015-11-28 23:06:27 +0700

    added more options with defaults

--- src/keyword_search/core.clj
+++ src/keyword_search/core.clj
@@ -8,16 +8,23 @@
 
 (def cli-options
   [["-s" "--sub" "list all the sub keywords (recursively)"]
+   ["-c" "--count" "Counts the results"]
+   ["-d" "--database DATABASE" "specifies database to use"
+    :default "soulflyer"]
+   ["-i" "--image-collection IMAGE-COLLECTION" "specifies the image collection"
+    :default "images"]
+   ["-k" "--keyword-collection KEYWORD-COLLECTION" "specifies the keyword collection"
+    :default "keywords"]
+   ["-m" "--metadata-field" "field to be searched"
+    :default :Keywords]
    ["-h" "--help"]])
 
-(defn find-images-with-keyword
-  "Searches database collection for entries containing keyword in the keyword array"
-  [database image-collection given-keyword]
+(defn find-images
+  "Searches database collection for entries where the given field is (or contains) the given value"
+  [database image-collection field value]
   (let [connection (mg/connect)
-        db (mg/get-db connection database)
-        images (mc/find-maps db image-collection {:Keywords given-keyword})]
-    images
-    ))
+        db (mg/get-db connection database)]
+    (mc/find-maps db image-collection {field value} [:_id])))
 
 (defn find-sub-keywords
   "given a keyword entry returns all the sub keywords"
@@ -35,20 +42,26 @@
 
 (defn -main [& args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
-        database (first arguments)
-        keyword-collection (second arguments)
-        image-collection (nth arguments 2)
-        given-keyword (nth arguments 3)]
+        ;;database (first arguments)
+        ;;keyword-collection (second arguments)
+        ;;image-collection (nth arguments 2)
+        given-keyword (first arguments)
+        ]
 
     (cond
      (:help options)
-     (println (str "Usage:\nkeyword-search [options] database keyword-coll image-coll keyword\n\noptions:\n" summary))
+     (println (str "Usage:\nkeyword-search [options] database keyword-coll image-coll keyword\n\nvoptions:\n" summary))
 
      (:sub options)
-     (find-sub-keywords database keyword-collection given-keyword)
+     (find-sub-keywords (:database options) (:keyword-collection options) given-keyword)
 
-     :else
+     (:count options)
      (println
       (str "Found "
-           (count (find-images-with-keyword database image-collection given-keyword))
-           " images.")))))
+           (count (find-images (:database options) (:image-collection options) (:metadata-field options) given-keyword))
+           " images."))
+
+     :else
+     (find-images (:database options) (:image-collection options) (:metadata-field options) given-keyword)
+     ;;(println "default case")
+     )))
