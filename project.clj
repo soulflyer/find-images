e377c5c260b7e6faa753baf04ecd828d6c80bca9
iain <iain@soulflyer.co.uk>
2015-12-14 09:24:54 +0700

    rename

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
                  [org.clojure/tools.cli "0.3.3"]
                  [com.novemberain/monger "3.0.1"]
                  [org.slf4j/slf4j-nop "1.7.12"]]
-  :main keyword-search.core
+  :main find-images.core
   :bin {:name "find-images"
         :bin-path "~/bin"})
